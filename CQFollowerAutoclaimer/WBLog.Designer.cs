72aae08f204f92f33dfd612e486b5f12da352b44
Wiedmolol <latas@wp.pl>
2018-04-24 12:12:08 +0200

    v0.6: Auto-WB, new settings file

---
+++ CQFollowerAutoclaimer/WBLog.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CQFollowerAutoclaimer
+{
+    partial class WBLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(489, 408);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label7.Location = new System.Drawing.Point(221, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "WB Log";
+            // 
+            // WBLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 460);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "WBLog";
+            this.Text = "WBLog";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WBLog_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ CQFollowerAutoclaimer/WBLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CQFollowerAutoclaimer
+{
+    public partial class WBLog : Form
+    {
+        public WBLog(ref string s)
+        {
+            InitializeComponent();
+            richTextBox1.Text = s;
+        }
+
+        private void WBLog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            this.Parent = null;
+            e.Cancel = true;
+        }
+        
+    }
+}
